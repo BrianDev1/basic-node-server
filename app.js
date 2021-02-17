3460bc9aa8d2d4d1933ec75b47bb646c71334001
BrianDev1 <brian.rudolph303@gmail.com>
2021-02-17 12:50:17 -0700

    improved

--- app.js
+++ app.js
@@ -24,11 +24,12 @@ app.get("/login/:name/:passphrase", (req, res) => {
   if(req.cookies.NodeServerSiteCookie === undefined) {
     res.cookie(`NodeServerSiteCookie`, `${req.params.name + req.params.passphrase}`, {maxAge: 100000, httpOnly: true});
     res.write(`Your Cookie was created successfully ${req.params.name}`);
+    res.write(`\nHead to /private and view the secret ${req.params.name}`);
     res.end();
     
   } else if (req.cookies.NodeServerSiteCookie) {
     res.write(`Welcome back ${req.params.name}`);
-    setTimeout(function(){ res.write("\nhead over to --> /private");res.end();}, 1000);
+     res.write("\nhead over to --> /private");res.end();
   }
 });
 
